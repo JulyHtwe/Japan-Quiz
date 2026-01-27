5819382e5cac84ed03cca58e4a58005f552c9c1f
July Htwe <julyhtwe6250@gmail.com>
2026-01-27 14:31:20 +0630

    home

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,70 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
-
+import { StyleSheet, ImageBackground, Text, View } from 'react-native';
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+    <ImageBackground
+      source={require('../../assets/images/bg_1.png')}
+      style={styles.bgImage}
+    >
+      <View>
+        <Text style={[styles.header, {marginTop: '20%'}]}>Hello Japan</Text>
+        <Text style={styles.header}>Quiz</Text>
+      </View>
+      <View>
+        <Text style= {[styles.middleText,{marginTop: '10%'}]}>Let's Learn</Text>
+        <Text style= {styles.middleText}>Japanese Vocab</Text>
+        <Text style= {styles.middleText}>The Fun Way!</Text>
+        <Text style= {[styles.middleText,{marginTop: '15%'}]}>Ready it test your</Text>
+        <Text style= {styles.middleText}>Brain</Text>
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+        <View style= {[styles.btn]}>
+          <Text style= {[styles.middleText,{color: 'black', fontSize: 30}]}>Start Quiz</Text>
+        </View>
+      </View>
+    </ImageBackground>
   );
 }
-
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  header: {
+    fontSize : 50,
+    color: 'black',
+    fontFamily: 'Kavoon',
+    textDecorationColor: 'white',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 5,
+    textShadowColor: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  middleText: {
+    fontSize : 35,
+    color: 'white',
+    fontFamily: 'Kavoon',
+    textDecorationColor: 'pink',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 5,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  bgImage: {
+    flex: 1,           
+    width: '100%',
+    height: '100%',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  btn:{
+    width: 300,
+    height: 70,
+    backgroundColor: 'white',
+    borderColor: 'pink',
+    borderWidth: 5,
+    justifyContent: 'center',
     alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+    borderRadius: 50,
+    marginTop: 30,
+    alignSelf: 'center'
+  }
 });
